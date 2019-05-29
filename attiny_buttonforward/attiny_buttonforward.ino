5d144f5fc5136a6f66dd8e25d52aac693e64f8de
Martin Sobotka <sobotkama@sobotkama.eu>
2019-05-29 19:56:34 +0200

    updated code for attiny

--- attiny_buttonforward/attiny_buttonforward.ino
+++ attiny_buttonforward/attiny_buttonforward.ino
@@ -3,34 +3,43 @@
 #define RX    1   // *** D1, Pin 2
 #define TX    2   // *** D2, Pin 1 I hope
 
-const int butt1 = 3;     // the number of the pushbutton pin
-const int butt2 = 4;     // the number of the LED pin
+#define butt1 4     // the number of the pushbutton pin
+#define butt2 3     // the number of the LED pin
 
-int butt1state;
-int butt2state;
-
+bool butt1state;
+bool butt2state;
+bool butt1pressed;
+bool butt2pressed;
 SoftwareSerial Serial(RX, TX);
 
 // variables will change:
-void setup() {      
+void setup() {
   // initialize the pushbutton pin as an input:
   pinMode(butt1, INPUT);
-  pinMode(butt2, INPUT);  
+  pinMode(butt2, INPUT);
   Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
   // read the state of the pushbutton value:
   butt1state = digitalRead(butt1);
   butt2state = digitalRead(butt2);
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (butt1state == LOW) {     
-    // send report    
-    Serial.print(1);  
-  } 
-  if (butt2state == LOW) {     
-    // send report    
-    Serial.print(0);  
-  } 
+  // check if the pushbuttons are pressed.
+  // if it is, the buttonState is LOW:
+  if (butt1state == LOW) {
+    // send report
+    if (butt1pressed == false) {
+      Serial.print(1,BIN);
+      butt1pressed = true;
+    }
+  } else if (butt2state == LOW) {
+    // send report
+    if (butt2pressed == false) {
+      Serial.print(0,BIN);
+      butt2pressed = true;
+    }
+  } else {
+    butt1pressed = false;
+    butt2pressed = false;
+  }
 }
